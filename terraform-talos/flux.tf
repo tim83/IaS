ac1aa0c83635eae2600938a400f79c19639004ac
Tim Mees <tim.mees83@gmail.com>
2024-08-06 21:45:11 +0200

    Enable auto updates in flux

--- terraform-talos/flux.tf
+++ terraform-talos/flux.tf
@@ -1,4 +1,5 @@
 resource "flux_bootstrap_git" "this" {
   embedded_manifests = true
   path               = "clusters/${var.cluster_name}"
+  components_extra = ["image-reflector-controller","image-automation-controller"]
 }
