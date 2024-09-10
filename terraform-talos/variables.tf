13959f05b25454a2f2b29e3694f4e7d81e1c458c
Tim Mees <tim.mees83@gmail.com>
2024-09-10 11:00:29 +0200

    Add coolermaster nodes

--- terraform-talos/variables.tf
+++ terraform-talos/variables.tf
@@ -96,6 +96,20 @@ variable "node_config" {
       cpu_count     = 6
       max_ram_gb    = 6
     },
+    {
+      pve_node_name = "coolermaster"
+      node_type     = "controller"
+      count         = 1
+      cpu_count     = 2
+      max_ram_gb    = 2
+    },
+    {
+      pve_node_name = "coolermaster"
+      node_type     = "worker"
+      count         = 1
+      cpu_count     = 2
+      max_ram_gb    = 6
+    },
   ]
 }
 
