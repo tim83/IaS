3cccfab296b9d5c16dbd2e3c37d14ce47ac3b9a5
Tim Mees <tim.mees83@gmail.com>
2024-09-11 20:06:42 +0200

    Remove flux image updater

--- terraform-talos/flux.tf
+++ terraform-talos/flux.tf
@@ -1,7 +1,6 @@
 resource "flux_bootstrap_git" "this" {
   embedded_manifests = true
   path               = "clusters/${var.cluster_name}"
-  components_extra   = ["image-reflector-controller", "image-automation-controller"]
 
   depends_on = [kubernetes_secret.sops-age]
 }
