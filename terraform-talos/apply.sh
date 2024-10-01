70ebf8fb4acad38a279910d1cc7386951f123037
Tim Mees <tim83@users.noreply.github.com>
2024-10-01 18:19:10 +0200

    Improve apply

--- terraform-talos/apply.sh
+++ terraform-talos/apply.sh
@@ -3,11 +3,8 @@
 # Install dependecies
 terraform init -upgrade || exit 1
 
-# Create plan
-terraform plan -out plan -var-file=secrets.tfvars || exit 1
-
 # Execute plan
-terraform apply plan || exit 1
+terraform apply || exit 1
 
 # Export outputs for use on this system
 terraform output -raw kubeconfig > ~/.kube/config
