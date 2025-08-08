992d0ed9c13110d467cf5100356da9dd4aaa85f8
Tim mees <git@mees.vip>
2025-08-08 10:01:47 +0200

    fixup! Add extra thinkcentre node

--- terraform-talos/variables.tf
+++ terraform-talos/variables.tf
@@ -103,15 +103,6 @@ variable "node_config" {
       disk_size     = 350
     },
     {
-      pve_node_name = "thinkcentre"
-      node_type     = "worker"
-      start_idx     = 0
-      count         = 1
-      cpu_count     = 4
-      max_ram_gb    = 4
-      disk_size     = 150
-    },
-    {
       pve_node_name = "coolermater"
       node_type     = "worker"
       start_idx     = 0
@@ -137,6 +128,15 @@ variable "node_config" {
       cpu_count     = 1
       max_ram_gb    = 2
       disk_size     = 60
+    },
+    {
+      pve_node_name = "thinkcentre"
+      node_type     = "worker"
+      start_idx     = 0
+      count         = 1
+      cpu_count     = 4
+      max_ram_gb    = 8
+      disk_size     = 150
     },
   ]
 }
