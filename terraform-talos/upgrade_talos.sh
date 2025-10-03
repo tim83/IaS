8be13447657ffe9b2de1575624d99fc6f967e65c
Tim mees <git@mees.vip>
2025-10-03 18:53:19 +0200

    Account for rpi controllers

--- terraform-talos/upgrade_talos.sh
+++ terraform-talos/upgrade_talos.sh
@@ -2,14 +2,22 @@
 
 control_node=$(echo "var.cluster_vip" | tofu console | jq -r)
 talos_version=$(echo "var.talos_version" | tofu console | jq -r)
-talos_factory_id=$(echo "var.talos_factory_id" | tofu console | jq -r)
+talos_vm_factory_id=$(echo "var.talos_factory_id" | tofu console | jq -r)
+talos_rpi_factory_id=$(echo "var.talos_rpi_factory_id" | tofu console | jq -r)
 k8s_version=$(echo "var.kubernetes_version" | tofu console | jq -r)
 
-talos_image="factory.talos.dev/nocloud-installer/${talos_factory_id}:v${talos_version}"
+talos_vm_image="factory.talos.dev/nocloud-installer/${talos_vm_factory_id}:v${talos_version}"
+talos_rpi_image="factory.talos.dev/metal-installer/${talos_rpi_factory_id}:v${talos_version}"
 
 nodes=$(talosctl get members -n $control_node -o json)
 for node_ip in $(echo $nodes | jq -r ".spec|if .operatingSystem|contains(\"${talos_version}\")|not then . else null end|.addresses|arrays|map(select(. != \"${control_node}\"))|first") ; do
-    echo $node_ip
+    hostname=$(talosctl get hostname -n $node_ip -o json | jq .spec.hostname)
+    echo "$hostname ($node_ip)"
+    if [[ $hostname =~ "rpi" ]]; then
+        talos_image=$talos_rpi_image
+    else
+        talos_image=$talos_vm_image
+    fi
     talosctl upgrade -n $node_ip --image $talos_image
 done
 
