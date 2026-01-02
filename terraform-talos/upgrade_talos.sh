9297ccf61fa262004c564a2610924896fdb6efdb
Tim mees <git@mees.vip>
2026-01-02 11:52:31 +0100

    Fix detecting RPI node

--- terraform-talos/upgrade_talos.sh
+++ terraform-talos/upgrade_talos.sh
@@ -10,15 +10,14 @@ talos_vm_image="factory.talos.dev/nocloud-installer-secureboot/${talos_vm_factor
 talos_rpi_image="factory.talos.dev/metal-installer/${talos_rpi_factory_id}:v${talos_version}"
 
 nodes=$(talosctl get members -n $control_node -o json)
-for node_ip in $(echo $nodes | jq -r ".spec|if .operatingSystem|contains(\"${talos_version}\")|not then . else null end|.addresses|arrays|map(select(. != \"${control_node}\"))|first") ; do
-    hostname=$(talosctl get hostname -n $node_ip -o json | jq .spec.hostname)
-    echo "$hostname ($node_ip)"
-    if [[ $hostname =~ "rpi" ]]; then
+for hostname in $(echo $nodes | jq -r ".spec|if .operatingSystem|contains(\"${talos_version}\")|not then . else null end|.hostname" | grep -v null) ; do
+    echo "$hostname"
+    if [[ $(talosctl -n $hostname get disk) =~ "mmcblk" ]]; then
         talos_image=$talos_rpi_image
     else
         talos_image=$talos_vm_image
     fi
-    talosctl upgrade -n $node_ip --image $talos_image
+    talosctl upgrade -n $hostname --image $talos_image
 done
 
 if talosctl health -n $control_node --wait-timeout 15s > /dev/null 2>&1; then
