d2a7cd6e7e2dfc2263fd1dcc673bf768391665ca
Tim mees <git@mees.vip>
2026-01-16 09:21:06 +0100

    Use new k8s objects

--- terraform-talos/flux.tf
+++ terraform-talos/flux.tf
@@ -2,10 +2,10 @@ resource "flux_bootstrap_git" "this" {
   embedded_manifests = true
   path               = "clusters/${var.cluster_name}"
 
-  depends_on = [kubernetes_secret.sops-age]
+  depends_on = [kubernetes_secret_v1.sops-age]
 }
 
-resource "kubernetes_namespace" "flux-system" {
+resource "kubernetes_namespace_v1" "flux-system" {
   metadata {
     name = "flux-system"
   }
@@ -18,7 +18,7 @@ resource "kubernetes_namespace" "flux-system" {
 
   depends_on = [time_sleep.wait_for_cluster_ip]
 }
-resource "kubernetes_secret" "sops-age" {
+resource "kubernetes_secret_v1" "sops-age" {
   metadata {
     name      = "sops-age"
     namespace = "flux-system"
@@ -28,5 +28,5 @@ resource "kubernetes_secret" "sops-age" {
     "age.agekey" = "${file("${path.module}/../../fluxcd/age.agekey")}"
   }
 
-  depends_on = [kubernetes_namespace.flux-system]
+  depends_on = [kubernetes_namespace_v1.flux-system]
 }
